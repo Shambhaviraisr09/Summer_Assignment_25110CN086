38ce1c008d222ae0d4466e0651688cda48d383e4
Shambhavi Rai <Shambhavirai.sr.09@gmail.com>
2026-06-16 12:58:25 +0530

    Day 16 uploaded

---
+++ day16/program2.java
@@ -0,0 +1,45 @@
+
+
+import java.util.Scanner;
+
+public class program2 {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+            int n= scnr.nextInt();
+            int a[]= new int[n];
+
+        System.out.println("Enter the array");
+        
+        for(int i=0; i<n;i++)
+        {
+            a[i]=scnr.nextInt();
+
+        }
+        int max=0;
+        int z=0;
+
+        for(int i=0; i<n;i++)
+            {
+
+                int b=0;
+                
+                for(int j=0; j<n; j++)
+                {
+                    if(a[i]==a[j])
+                        b++;
+                }
+
+                if(b>max)
+                   { max=b;
+                    z=a[i];
+                   }
+
+
+            }
+            System.out.println("The number with max frequency is "+z);
+            scnr.close();
+
+    }
+    
+}
